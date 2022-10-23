8c8af32e0bd7d198aef0359e7bd18fd304ae6297
Injoon Hwang <injoon0617@gmail.com>
2022-10-23 23:13:05 +0900

    ✨ add routes

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+
+  border-bottom: 1px solid #ffffff;
+
+  & > a {
+    flex: 1;
+
+    padding: 1rem;
+
+    text-align: center;
+
+    &:hover {
+      background-color: #424242;
+    }
+  }
+`;
 
 export const MainLayout: React.FC = () => {
-  return <div>Main Layout</div>;
+  return (
+    <div>
+      <Nav>
+        <Link to="ideaInfo/1">Idea Info</Link>
+        <hr />
+        <Link to="linkInfo/1">Link Info</Link>
+        <hr />
+        <Link to="linkedIdeas/1">Linked Ideas</Link>
+        <hr />
+        <Link to="ideaLinker/1">Idea Linker</Link>
+        <hr />
+        <Link to="ideaFactory">Node Factory</Link>
+        <hr />
+        <Link to="linkFactory">Link Factory</Link>
+      </Nav>
+      <Outlet />
+    </div>
+  );
 };
--- src/router.tsx
+++ src/router.tsx
@@ -11,6 +11,32 @@ const router = createBrowserRouter([
     path: "/",
     element: <MainLayout />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "ideaInfo/:ideaId",
+        element: <div>Idea Info</div>,
+      },
+      {
+        path: "linkInfo/:linkId",
+        element: <div>Link Info</div>,
+      },
+      {
+        path: "linkedIdeas/:ideaId",
+        element: <div>Linked Ideas</div>,
+      },
+      {
+        path: "ideaLinker/:ideaId",
+        element: <div>Idea Linker</div>,
+      },
+      {
+        path: "ideaFactory",
+        element: <div>Node Factory</div>,
+      },
+      {
+        path: "linkFactory",
+        element: <div>Link Factory</div>,
+      },
+    ],
   },
 ]);
 
