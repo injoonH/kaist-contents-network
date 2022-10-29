12ec21afadda670b5eb4df9e5ce284dd5087e1b3
Injoon Hwang <injoon0617@gmail.com>
2022-10-29 09:31:31 +0900

    ✨ open on localhost using chrome automatically

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
   plugins: [react()],
