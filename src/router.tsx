a00bfdb441948d5fbafe8a6894a91ae94055229f
Injoon Hwang <injoon0617@gmail.com>
2022-10-30 20:23:14 +0900

    ♻️ use empty tags for index children

--- src/router.tsx
+++ src/router.tsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <div>blank</div>,
+        element: <></>,
       },
       {
         path: "ideaInfo/:ideaId",
