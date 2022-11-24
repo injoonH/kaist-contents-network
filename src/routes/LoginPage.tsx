15579d6c4ef06b6f5c74c54e45933bc86f1e5c5c
Injoon Hwang <injoon0617@gmail.com>
2022-11-24 21:47:22 +0900

    ✨ add particles in the background of a login page

--- src/routes/LoginPage.tsx
+++ src/routes/LoginPage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import type { Engine } from "tsparticles-engine";
 import styled from "styled-components";
 import { colors, fonts } from "@/theme";
 
@@ -30,7 +33,7 @@ const Description = styled.p`
   max-width: clamp(50ch, 60%, 64ch);
 
   font-size: 1.6rem;
-  color: ${colors.gray_400};
+  color: ${colors.gray_350};
 `;
 
 const ButtonContainer = styled.div`
@@ -58,8 +61,85 @@ const ButtonContainer = styled.div`
 export const LoginPage: React.FC = () => {
   const { t, i18n } = useTranslation();
 
+  const particlesInit = React.useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
   return (
     <>
+      <Particles
+        init={particlesInit}
+        options={{
+          background: {
+            color: {
+              value: colors.gray_900,
+            },
+            position: "absolute",
+          },
+          fpsLimit: 120,
+          fullScreen: {
+            zIndex: -1,
+          },
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 150,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: colors.white,
+            },
+            links: {
+              color: colors.white,
+              distance: 150,
+              enable: true,
+              opacity: 0.3,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.3,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 4 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <Container>
         <Title>
           <span>KAIST</span> Contents Network
